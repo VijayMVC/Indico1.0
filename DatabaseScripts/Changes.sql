77b51c388a7b9189501f02ff9c673a2ea4ae074a
rusith <rusith@bourketechnologies.com>
2016-09-27 09:48:56 +0530

    SP Change for update Label in related OrderDetails when transferring a Job Name Or Distributor

--- DatabaseScripts/Changes.sql
+++ DatabaseScripts/Changes.sql
@@ -22,13 +22,13 @@ AS
 		BEGIN
 			DECLARE @Label int
 			SET @Label=(SELECT TOP 1 dl.Label 
-							FROM [dbo].[DistributorLabel] dl WHERE dl.Distributor = @ToDistributor)
+						FROM [dbo].[DistributorLabel] dl WHERE dl.Distributor = @ToDistributor)
 
 			UPDATE od SET Label = @Label
-				FROM [dbo].[Order] o
-					INNER JOIN [dbo].[OrderDetail] od
-						ON od.[Order] = o.ID
-				WHERE Distributor = @Distributor
+			FROM [dbo].[Order] o
+				INNER JOIN [dbo].[OrderDetail] od
+					ON od.[Order] = o.ID
+			WHERE Distributor = @Distributor
 
 			UPDATE [dbo].[Client] SET Distributor = @ToDistributor
 			WHERE Distributor = @Distributor
@@ -36,5 +36,73 @@ AS
 	END
 
 GO
+
+--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[SPC_TransferJobName]') AND type in (N'P', N'PC'))
+      DROP PROCEDURE [dbo].[SPC_TransferJobName]
+GO
+
+CREATE PROC [dbo].[SPC_TransferJobName]
+	@P_JobName int,
+	@P_Distributor int
+AS
+BEGIN
+	DECLARE @Client int 
+	DECLARE @ClientDistributor int 
+	DECLARE @ClientName nvarchar(64)
+
+	SET @Client = (SELECT TOP 1 cl.ID FROM [dbo].[Client] cl 
+						INNER JOIN [dbo].[JobName] jn
+							ON cl.ID = jn.Client
+				   WHERE jn.ID = @P_JobName)
+
+	SET @ClientDistributor = (SELECT TOP 1 d.ID 
+							FROM [dbo].[Client] c
+								INNER JOIN [dbo].[Company] d
+									ON c.Distributor = d.ID
+							WHERE c.ID = @Client)
+
+	SET @ClientName = (SELECT TOP 1 cl.Name FROM [dbo].[Client] cl 
+						    WHERE cl.ID = @Client)
+
+
+
+	IF NOT EXISTS (SELECT cl.ID FROM [dbo].[Company] di
+							INNER JOIN  [dbo].[Client] cl
+								ON cl.Distributor = di.ID
+			   WHERE cl.Name = @ClientName AND di.ID = @P_Distributor)
+	BEGIN
+		INSERT INTO [dbo].[Client] (Name,Distributor,[Description],FOCPenalty)
+			SELECT TOP 1 Name,@P_Distributor,[Description],FOCPenalty 
+			FROM [dbo].[Client] 
+			WHERE ID = @Client
+		SET @Client = SCOPE_IDENTITY()
+	END
+	ELSE 
+	BEGIN
+		SET @Client = (SELECT TOP 1 cl.ID FROM [dbo].[Company] di
+							INNER JOIN  [dbo].[Client] cl
+								ON cl.Distributor = di.ID
+			   WHERE cl.Name = @ClientName AND di.ID = @P_Distributor)
+	END
+
+	DECLARE @Label int
+
+	SET @Label=(SELECT TOP 1 dl.Label 
+					FROM [dbo].[DistributorLabel] dl WHERE dl.Distributor = @P_Distributor)
+
+	UPDATE  od SET Label = @Label
+	FROM [dbo].[JobName] jn
+		INNER JOIN [dbo].[Order] o
+			ON o.Client = jn.ID
+		INNER JOIN [dbo].[OrderDetail] od
+			ON od.[Order] = o.ID
+	WHERE jn.ID = @P_JobName
+
+	UPDATE [dbo].[JobName] SET Client = @Client WHERE ID = @P_JobName
+END
+GO
+
 
 --**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--
