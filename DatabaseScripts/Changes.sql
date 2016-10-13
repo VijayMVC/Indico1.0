45c7a29381a53b2a3a0fed19b6ac253666873998
rusith <rusith@bourketechnologies.com>
2016-10-13 16:46:46 +0530

    removed unwanted changes

--- DatabaseScripts/Changes.sql
+++ DatabaseScripts/Changes.sql
@@ -3,104 +3,6 @@ GO
 
 --**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--
 
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[SPC_TransferDistributor]') AND type in (N'P', N'PC'))
-      DROP PROCEDURE [dbo].[SPC_TransferDistributor]
-GO
-
-CREATE PROCEDURE [dbo].[SPC_TransferDistributor]
-	@P_FromDistributor int,
-	@P_ToDistributor int
-AS
-	BEGIN
-		IF (@P_FromDistributor > 0 AND @P_ToDistributor > 0)
-		BEGIN
-			DECLARE @Label int
-			SET @Label=(SELECT TOP 1 dl.Label 
-						FROM [dbo].[DistributorLabel] dl WHERE dl.Distributor = @P_ToDistributor)
-
-			UPDATE od SET Label = @Label
-			FROM [dbo].[Order] o
-				INNER JOIN [dbo].[OrderDetail] od
-					ON od.[Order] = o.ID
-			WHERE Distributor = @P_FromDistributor
-
-			UPDATE [dbo].[Client] SET Distributor = @P_ToDistributor
-			WHERE Distributor = @P_FromDistributor
-		END
-	END
-
-GO
-
---**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--
-
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[SPC_TransferJobName]') AND type in (N'P', N'PC'))
-      DROP PROCEDURE [dbo].[SPC_TransferJobName]
-GO
-
-CREATE PROC [dbo].[SPC_TransferJobName]
-	@P_JobName int,
-	@P_Distributor int
-AS
-BEGIN
-	DECLARE @Client int 
-	DECLARE @ClientDistributor int 
-	DECLARE @ClientName nvarchar(64)
-
-	SET @Client = (SELECT TOP 1 cl.ID FROM [dbo].[Client] cl 
-						INNER JOIN [dbo].[JobName] jn
-							ON cl.ID = jn.Client
-				   WHERE jn.ID = @P_JobName)
-
-	SET @ClientDistributor = (SELECT TOP 1 d.ID 
-							FROM [dbo].[Client] c
-								INNER JOIN [dbo].[Company] d
-									ON c.Distributor = d.ID
-							WHERE c.ID = @Client)
-
-	SET @ClientName = (SELECT TOP 1 cl.Name FROM [dbo].[Client] cl 
-						    WHERE cl.ID = @Client)
-
-
-
-	IF NOT EXISTS (SELECT cl.ID FROM [dbo].[Company] di
-							INNER JOIN  [dbo].[Client] cl
-								ON cl.Distributor = di.ID
-			   WHERE cl.Name = @ClientName AND di.ID = @P_Distributor)
-	BEGIN
-		INSERT INTO [dbo].[Client] (Name,Distributor,[Description],FOCPenalty)
-			SELECT TOP 1 Name,@P_Distributor,[Description],FOCPenalty 
-			FROM [dbo].[Client] 
-			WHERE ID = @Client
-		SET @Client = SCOPE_IDENTITY()
-	END
-	ELSE 
-	BEGIN
-		SET @Client = (SELECT TOP 1 cl.ID FROM [dbo].[Company] di
-							INNER JOIN  [dbo].[Client] cl
-								ON cl.Distributor = di.ID
-			   WHERE cl.Name = @ClientName AND di.ID = @P_Distributor)
-	END
-
-	DECLARE @Label int
-
-	SET @Label=(SELECT TOP 1 dl.Label 
-					FROM [dbo].[DistributorLabel] dl WHERE dl.Distributor = @P_Distributor)
-
-	UPDATE  od SET Label = @Label
-	FROM [dbo].[JobName] jn
-		INNER JOIN [dbo].[Order] o
-			ON o.Client = jn.ID
-		INNER JOIN [dbo].[OrderDetail] od
-			ON od.[Order] = o.ID
-	WHERE jn.ID = @P_JobName
-
-	UPDATE [dbo].[JobName] SET Client = @Client WHERE ID = @P_JobName
-END
-GO
-
-
---**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--
-
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[SPC_GetBackOrdersWeeklySummary]') AND type in (N'P', N'PC'))
       DROP PROCEDURE [dbo].[SPC_GetBackOrdersWeeklySummary]
 GO
@@ -224,94 +126,6 @@ GO
 
 --**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--
 
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[SPC_TransferDistributor]') AND type in (N'P', N'PC'))
-      DROP PROCEDURE [dbo].[SPC_TransferDistributor]
-GO
-
-
-CREATE PROCEDURE [dbo].[SPC_TransferDistributor]
-	@P_FromDistributor int,
-	@P_ToDistributor int
-AS
-	BEGIN
-		IF (@P_FromDistributor > 0 AND @P_ToDistributor > 0)
-		BEGIN
-			DECLARE @Label int
-			SET @Label=(SELECT TOP 1 dl.Label 
-						FROM [dbo].[DistributorLabel] dl WHERE dl.Distributor = @P_ToDistributor)
-
-
-			UPDATE [dbo].[Client] SET Distributor = @P_ToDistributor
-			WHERE Distributor = @P_FromDistributor
-		END
-	END
-
-GO
-
---**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--
-
-
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[SPC_TransferJobName]') AND type in (N'P', N'PC'))
-      DROP PROCEDURE [dbo].[SPC_TransferJobName]
-GO
-
-
-CREATE PROC [dbo].[SPC_TransferJobName]
-	@P_JobName int,
-	@P_Distributor int
-AS
-BEGIN
-	DECLARE @Client int 
-	DECLARE @ClientDistributor int 
-	DECLARE @ClientName nvarchar(64)
-
-	SET @Client = (SELECT TOP 1 cl.ID FROM [dbo].[Client] cl 
-						INNER JOIN [dbo].[JobName] jn
-							ON cl.ID = jn.Client
-				   WHERE jn.ID = @P_JobName)
-
-	SET @ClientDistributor = (SELECT TOP 1 d.ID 
-							FROM [dbo].[Client] c
-								INNER JOIN [dbo].[Company] d
-									ON c.Distributor = d.ID
-							WHERE c.ID = @Client)
-
-	SET @ClientName = (SELECT TOP 1 cl.Name FROM [dbo].[Client] cl 
-						    WHERE cl.ID = @Client)
-
-
-
-	IF NOT EXISTS (SELECT cl.ID FROM [dbo].[Company] di
-							INNER JOIN  [dbo].[Client] cl
-								ON cl.Distributor = di.ID
-			   WHERE cl.Name = @ClientName AND di.ID = @P_Distributor)
-	BEGIN
-		INSERT INTO [dbo].[Client] (Name,Distributor,[Description],FOCPenalty)
-			SELECT TOP 1 Name,@P_Distributor,[Description],FOCPenalty 
-			FROM [dbo].[Client] 
-			WHERE ID = @Client
-		SET @Client = SCOPE_IDENTITY()
-	END
-	ELSE 
-	BEGIN
-		SET @Client = (SELECT TOP 1 cl.ID FROM [dbo].[Company] di
-							INNER JOIN  [dbo].[Client] cl
-								ON cl.Distributor = di.ID
-			   WHERE cl.Name = @ClientName AND di.ID = @P_Distributor)
-	END
-
-	DECLARE @Label int
-
-	SET @Label=(SELECT TOP 1 dl.Label 
-					FROM [dbo].[DistributorLabel] dl WHERE dl.Distributor = @P_Distributor)
-
-	UPDATE [dbo].[JobName] SET Client = @Client WHERE ID = @P_JobName
-END
-
-GO
-
---**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--
-
 ALTER TABLE [dbo].[Label]
 ADD IsActive bit
 
@@ -414,12 +228,12 @@ GO
 
 --**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--
 
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[SPC_TransferJobName]') AND type in (N'P', N'PC'))
-      DROP PROCEDURE [dbo].[SPC_TransferJobName]
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[SPC_TransferVisualLayout]') AND type in (N'P', N'PC'))
+      DROP PROCEDURE [dbo].[SPC_TransferVisualLayout]
 GO
 
 
-ALTER PROC [dbo].[SPC_TransferVisualLayout]
+CREATE PROC [dbo].[SPC_TransferVisualLayout]
 	@P_VisualLayout int,
 	@P_JobName int
 AS
