308bc8c42d1b1337cafbda322c2a8826ba80c970
sachitu <sachith@bourketechnologies.com>
2016-10-24 18:31:39 +0530

    clone_order page added

---
+++ Indico/CloneOrder.aspx
@@ -0,0 +1,112 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Indico.Master" AutoEventWireup="true" EnableEventValidation="false"
+    CodeBehind="CloneOrder.aspx.cs" Inherits="Indico.CloneOrder" MaintainScrollPositionOnPostback="true" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="iContentPlaceHolder" runat="server">
+    <!-- Page -->
+    <div class="page">
+        <!-- Page Header -->
+        <div class="page-header">
+            <div class="header-actions">
+            </div>
+            <h3>Clone Order</h3>
+        </div>
+        <!-- / -->
+        <!-- Page Content -->
+        <div class="page-content">
+            <asp:ValidationSummary ID="validationSummary" runat="server" CssClass="alert alert-danger"
+                ValidationGroup="valGrpClone" DisplayMode="BulletList" HeaderText="<strong>Errors were encountered while trying to process the form below</strong>"></asp:ValidationSummary>
+              <div class="control-group">
+                <label class="control-label">
+                    Distributor</label>
+                <div class="controls">
+                    <asp:Label ID="lblDistributor" runat="server"></asp:Label>
+                </div>
+            </div>
+            <div class="control-group">
+                <label class="control-label">
+                    Client</label>
+                <div class="controls">
+                    <asp:Label ID="lblClient" runat="server"></asp:Label>
+                </div>
+            </div>
+            <div class="control-group">
+                <label class="control-label">
+                    Job Name</label>
+                <div class="controls">
+                    <asp:Label ID="lblJobName" runat="server"></asp:Label>
+                </div>
+            </div>
+            <div class="control-group">
+                <label class="control-label">
+                    Billing Address</label>
+                <div class="controls">
+                    <asp:Label ID="lblBillingAddress" runat="server"></asp:Label>
+                </div>
+            </div>
+            <div class="control-group">
+                <label class="control-label">
+                    Despatch Address</label>
+                <div class="controls">
+                    <asp:Label ID="lblDespatchAddress" runat="server"></asp:Label>
+                </div>
+            </div>
+            <div class="control-group">
+                <label class="control-label">
+                    Date Required in Customers Hand</label>
+                <div class="controls">
+                    <asp:Label ID="lblDateRequiredinCustomersHand" runat="server"></asp:Label>
+                </div>
+            </div>
+            <div class="control-group">
+                <label class="control-label">
+                    Shipment Date</label>
+                <div class="controls">
+                    <asp:Label ID="lblShipmentDate" runat="server"></asp:Label>
+                </div>
+            </div>
+            <div class="control-group">
+                <label class="control-label">
+                    Shipment Term</label>
+                <div class="controls">
+                    <asp:Label ID="lblShipmentTerm" runat="server"></asp:Label>
+                </div>
+            </div>
+            <div class="control-group">
+                <label class="control-label">
+                    Shipment Mode</label>
+                <div class="controls">
+                    <asp:Label ID="lblShipmentMode" runat="server"></asp:Label>
+                </div>
+            </div>
+        </div>
+        <div class="page-content">
+            <asp:Repeater ID="rptOrderDetails" runat="server" OnItemDataBound="rptOrderDetails_ItemDataBound" EnableViewState="true">
+                <ItemTemplate>
+                    <div class="controls">
+                        <asp:CheckBox ID="chkOrderDetail" runat="server"></asp:CheckBox>
+                        <asp:HiddenField ID="hdnODID" runat="server" Value="0" />
+                    </div>
+                    <div class="controls">
+                        <asp:Label ID="lblVisualLayout" runat="server"></asp:Label>
+                    </div>
+                    <div class="controls">
+                        <a id="ancVLImage" runat="server"><i id="ivlimageView" runat="server" class="icon-eye-open"></i></a>
+                    </div>
+                </ItemTemplate>
+            </asp:Repeater>
+            <asp:CustomValidator ID="cvOrderDetails" runat="server" ValidationGroup="valGrpClone" OnServerValidate="cvOrderDetails_ServerValidate" ErrorMessage="Visual Layout is required"
+                ToolTip="Visual Layout is required">
+                <img src="Content/img/icon_warning.png" style="padding-left:180px"  title="Visual Layout is required" alt="Visual Layout is required" />
+            </asp:CustomValidator>
+        </div>
+        <div class="form-actions">
+            <button id="btnSaveChanges" runat="server" class="btn btn-primary" onserverclick="btnSaveChanges_Click"
+                data-loading-text="Saving..." type="submit" validationgroup="valGrpClone">
+                Save Changes
+            </button>
+            <button id="btnClose" runat="server" class="btn btn-default" type="submit" causesvalidation="false" onserverclick="btnClose_ServerClick">
+                Cancel                                       
+            </button>
+        </div>
+    </div>
+</asp:Content>
---
+++ Indico/CloneOrder.aspx.cs
@@ -0,0 +1,183 @@
+﻿using Dapper;
+using Indico.BusinessObjects;
+using Indico.Common;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace Indico
+{
+    public partial class CloneOrder : IndicoPage
+    {
+        #region Fields
+
+        private int urlQueryID = -1;
+
+        #endregion
+
+        #region Properties
+
+        protected int QueryID
+        {
+            get
+            {
+                if (urlQueryID > -1)
+                    return urlQueryID;
+
+                urlQueryID = 0;
+                if (Request.QueryString["id"] != null)
+                {
+                    urlQueryID = Convert.ToInt32(Request.QueryString["id"].ToString());
+                }
+
+                return urlQueryID;
+            }
+        }
+
+        #endregion
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                if (this.QueryID > 0)
+                {
+                    OrderBO objOrder = new OrderBO();
+                    objOrder.ID = this.QueryID;
+                    objOrder.GetObject();
+
+                    OrderDetailBO objOrderDetail = new OrderDetailBO();
+                    objOrderDetail.Order = objOrder.ID;
+                    List<OrderDetailBO> lstOrderDetails = objOrderDetail.SearchObjects();
+                    objOrderDetail = lstOrderDetails.First();
+
+                    this.lblDistributor.Text = objOrder.objDistributor.Name;
+                    this.lblJobName.Text = objOrder.objClient.Name;
+                    this.lblClient.Text = objOrder.objClient.objClient.Name;
+                    this.lblDespatchAddress.Text = objOrder.objDespatchToAddress.CompanyName + "  " + objOrder.objDespatchToAddress.Address + "  "
+                                                    + objOrder.objDespatchToAddress.PostCode + "  " + objOrder.objDespatchToAddress.Suburb + "  "
+                                                    + objOrder.objDespatchToAddress.PostCode + "  " + objOrder.objDespatchToAddress.State + "  "
+                                                    + objOrder.objDespatchToAddress.objCountry.ShortName; ;
+
+                    this.lblBillingAddress.Text = objOrder.objBillingAddress.CompanyName + "  " + objOrder.objBillingAddress.Address + "  "
+                                                    + objOrder.objBillingAddress.PostCode + "  " + objOrder.objBillingAddress.Suburb + "  "
+                                                    + objOrder.objBillingAddress.PostCode + "  " + objOrder.objBillingAddress.State + "  "
+                                                    + objOrder.objBillingAddress.objCountry.ShortName;
+                    int processingPeriod = 5;
+                    int.TryParse(IndicoPage.GetSetting("OPP"), out processingPeriod);
+                    lblDateRequiredinCustomersHand.Text = IndicoPage.GetNextWeekday(DayOfWeek.Monday).AddDays(7 * (processingPeriod - 1)).ToString("dd MMMM yyyy");
+
+
+                    int.TryParse(IndicoPage.GetSetting("OPP"), out processingPeriod);
+                    lblShipmentDate.Text = IndicoPage.GetNextWeekday(DayOfWeek.Monday).AddDays(7 * (processingPeriod - 1) - 2).ToString("dd MMMM yyyy");
+
+                    this.lblShipmentTerm.Text = objOrderDetail.objPaymentMethod.Name;
+                    this.lblShipmentMode.Text = objOrderDetail.objShipmentMode.Name;
+
+                    this.rptOrderDetails.DataSource = lstOrderDetails;
+                    this.rptOrderDetails.DataBind();
+                }
+            }
+        }
+        protected void rptOrderDetails_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            RepeaterItem item = e.Item;
+            if (item.ItemIndex > -1 && item.DataItem is OrderDetailBO)
+            {
+                OrderDetailBO objOrderDetail = (OrderDetailBO)item.DataItem;
+
+                CheckBox chkOrderDetail = (CheckBox)item.FindControl("chkOrderDetail");
+                Label lblVisualLayout = (Label)item.FindControl("lblVisualLayout");
+                lblVisualLayout.Text = objOrderDetail.objVisualLayout.NamePrefix;
+                HtmlAnchor ancVLImage = (HtmlAnchor)item.FindControl("ancVLImage");
+                HtmlGenericControl ivlimageView = (HtmlGenericControl)item.FindControl("ivlimageView");
+
+                if (!string.IsNullOrEmpty(ancVLImage.HRef))
+                {
+                    ancVLImage.Attributes.Add("class", "btn-link preview");
+                    ivlimageView.Attributes.Add("class", "icon-eye-open");
+
+                    List<float> lstVLImageDimensions = (new ImageProcess()).GetResizedImageDimension(960, 720, 420, 360);
+                    if (lstVLImageDimensions.Count > 0)
+                    {
+                        ancVLImage.Attributes.Add("height", lstVLImageDimensions[0].ToString());
+                        ancVLImage.Attributes.Add("width", lstVLImageDimensions[1].ToString());
+                    }
+                }
+                else
+                {
+                    ancVLImage.Title = "Visual Layout Image Not Available";
+                    ivlimageView.Attributes.Add("class", "icon-eye-close");
+                }
+
+                HiddenField ODID = (HiddenField)item.FindControl("hdnODID");
+                ODID.Value = objOrderDetail.ID.ToString();
+            }
+        }
+
+        protected void btnSaveChanges_Click(object sender, EventArgs e)
+        {
+            if (Page.IsValid)
+            {
+                try
+                {
+                    string orderDetailIDs = string.Empty;
+
+                    foreach (RepeaterItem item in rptOrderDetails.Items)
+                    {
+                        CheckBox chkOrderDetail = (CheckBox)item.FindControl("chkOrderDetail");
+                        HiddenField ODID = (HiddenField)item.FindControl("hdnODID");
+
+                        if (chkOrderDetail.Checked)
+                            orderDetailIDs += ODID.Value + ",";
+                    }
+
+                    int success = 0;
+
+                    System.Data.SqlClient.SqlConnection connection = new System.Data.SqlClient.SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+                    connection.Open();
+
+                    int orderID = this.QueryID;
+
+                    string query = string.Format(@"EXEC [Indico].[dbo].[SPC_CloneOrder] '{0}','{1}','{2}'",
+                                                                                         this.LoggedUser.ID, orderID, orderDetailIDs);
+                    var result = connection.Query<int>(query);
+                    connection.Close();
+
+                    success = result.First();
+
+                    // Response.Redirect("/ViewOrders.aspx");
+
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+        }
+
+        protected void btnClose_ServerClick(object sender, EventArgs e)
+        {
+            Response.Redirect("/ViewOrders.aspx");
+        }
+
+        protected void cvOrderDetails_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            args.IsValid = false;
+
+            foreach (RepeaterItem item in rptOrderDetails.Items)
+            {
+                CheckBox chkOrderDetail = (CheckBox)item.FindControl("chkOrderDetail");
+
+                if (chkOrderDetail.Checked)
+                {
+                    args.IsValid = true;
+                    break;
+                }
+            }
+        }
+    }
+}
