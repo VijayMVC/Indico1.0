aaf20d84448ff2898a608a3afe6f63062f398962
rusith <rusith@bourketechnologies.com>
2016-10-25 13:10:52 +0530

    Added new SP (SPC_UpdateOrderStatusToShipped) . this will call from the packing application

--- DatabaseScripts/Changes.sql
+++ DatabaseScripts/Changes.sql
@@ -2,3 +2,34 @@ USE Indico
 GO
 
 --**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[SPC_UpdateOrderStatusToShipped]') AND type in (N'P', N'PC'))
+      DROP PROCEDURE [dbo].[SPC_UpdateOrderStatusToShipped]
+GO
+
+CREATE PROC [dbo].[SPC_UpdateOrderStatusToShipped]
+	@P_OrderDetailIds nvarchar(max)
+AS
+BEGIN
+	DECLARE @ODs TABLE ( ID int )
+	INSERT INTO @ODs (ID) SELECT DATA FROM [dbo].Split(@P_OrderDetailIds,',')
+	
+	UPDATE od
+	SET od.[Status] = 16 --Shipped
+	FROM [dbo].[OrderDetail] od
+		INNER JOIN @ODs ods
+			ON ods.ID = od.ID
+
+	UPDATE o
+	SET o.[Status] = 21 --Completed
+	FROM [dbo].[OrderDetail] od
+		INNER JOIN @ODs ods
+			ON ods.ID = od.ID
+		INNER JOIN [dbo].[Order] o
+			ON od.[Order] = o.ID
+
+END
+
+GO
+
+--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--
