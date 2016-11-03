f90bc4a7a016da4eb828fdaab9c704db0b2944cc
DESKTOP-J9ABQPM\Pumbaa <danesh@bourketechnologies.com>
2016-11-03 16:48:34 +0530

    -- access to clone order page..

--- DatabaseScripts/Changes.sql
+++ DatabaseScripts/Changes.sql
@@ -3,10 +3,20 @@ GO
 
 --**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--
 
--- correction for cost sheet changes
+-- access to clone orders for direct sales (Indico Coordinators)
 
-UPDATE [dbo].[VisualLayout] SET FabricCode = '424' WHERE Pattern = 627
-
+INSERT INTO [dbo].[MenuItemRole] VALUES( 												
+											(	SELECT ID
+												FROM [dbo].[MenuItem]
+												WHERE [Page] = (	SELECT ID
+																	FROM [dbo].[Page]
+																	WHERE name = '/CloneOrder.aspx'
+																) 																	
+												),
+												(	SELECT ID FROM [dbo].[Role]
+													WHERE [Description] = 'Indico Coordinator'	
+												)
+										)
 GO
 
 --**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--
