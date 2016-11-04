39cb02304babc0d90d40ca763283cdf4db4fe774
DESKTOP-J9ABQPM\Pumbaa <danesh@bourketechnologies.com>
2016-11-04 15:20:22 +0530

    -- stamped Changes.sql

--- DatabaseScripts/Changes.sql
+++ DatabaseScripts/Changes.sql
@@ -3,20 +3,6 @@ GO
 
 --**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--
 
--- access to clone orders for direct sales (Indico Coordinators)
 
-INSERT INTO [dbo].[MenuItemRole] VALUES( 												
-											(	SELECT ID
-												FROM [dbo].[MenuItem]
-												WHERE [Page] = (	SELECT ID
-																	FROM [dbo].[Page]
-																	WHERE name = '/CloneOrder.aspx'
-																) 																	
-												),
-												(	SELECT ID FROM [dbo].[Role]
-													WHERE [Description] = 'Indico Coordinator'	
-												)
-										)
-GO
 
 --**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--
