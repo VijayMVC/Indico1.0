788a2a00e3d80c5263ddaeb7d2893073cdec860a
DESKTOP-J9ABQPM\Pumbaa <danesh@bourketechnologies.com>
2016-11-25 18:24:32 +0530

    -- errors..

--- Indico/AddEditOrder.aspx.designer.cs
+++ Indico/AddEditOrder.aspx.designer.cs
@@ -490,15 +490,6 @@ namespace Indico {
         protected global::System.Web.UI.WebControls.TextBox txtNotes;
         
         /// <summary>
-        /// dvOfficeUse control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl dvOfficeUse;
-        
-        /// <summary>
         /// txtShipmentDate control.
         /// </summary>
         /// <remarks>
