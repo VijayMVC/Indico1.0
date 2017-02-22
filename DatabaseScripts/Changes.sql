0cf10e850d4827fcfdf0469a07e382d7afc18665
DESKTOP-J9ABQPM\Pumbaa <danesh@bourketechnologies.com>
2017-02-22 18:46:07 +0530

    -- sp changes for drill down reports..

--- DatabaseScripts/Changes.sql
+++ DatabaseScripts/Changes.sql
@@ -3,68 +3,75 @@ GO
 
 --**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--
 
-/****** Object:  StoredProcedure [dbo].[SPC_GetOrderDetailIndicoPrice]    Script Date: 1/26/2017 1:31:47 PM ******/
-DROP PROCEDURE [dbo].[SPC_GetOrderDetailIndicoPrice]
+/****** Object:  StoredProcedure [dbo].[SPC_GetDetailReportByClient]    Script Date: 2/22/2017 6:22:22 PM ******/
+DROP PROCEDURE [dbo].[SPC_GetDetailReportByClient]
 GO
 
-/****** Object:  StoredProcedure [dbo].[SPC_GetOrderDetailIndicoPrice]    Script Date: 1/26/2017 1:31:47 PM ******/
+/****** Object:  StoredProcedure [dbo].[SPC_GetDetailReportByClient]    Script Date: 2/22/2017 6:22:22 PM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE PROCEDURE [dbo].[SPC_GetOrderDetailIndicoPrice]
-(
-	@P_Order AS int
+CREATE PROC [dbo].[SPC_GetDetailReportByClient](
+	@P_StartDate AS datetime2(7) = '20160101',
+	@P_EndDate AS datetime2(7) = '20160301',
+	@P_Distributor AS int,
+	@P_Client AS int
 )
-AS 
+AS
+BEGIN
 
-BEGIN	
-	SELECT 	
-		  od.[ID] AS OrderDetail
-		  ,ot.[Name] AS OrderType
-		  ,ISNULL(vl.[NamePrefix] + ''+ ISNULL(CAST(vl.[NameSuffix] AS NVARCHAR(64)), ''),'') AS VisualLayout
-		  ,ISNULL(od.[VisualLayout],0) AS VisualLayoutID		  
-		  ,ISNULL(p.Number, (SELECT Number FROM Pattern WHERE ID = od.[Pattern])) AS Pattern 
-		  ,ISNULL(p.ID, od.[Pattern]) AS PatternID
-		  ,ISNULL(fc.ID, od.FabricCode) AS FabricID
-		  ,ISNULL(fc.[Name], (SELECT Name FROM FabricCode WHERE ID = od.FabricCode)) AS Fabric 		  
-		  ,od.[Order] AS 'Order'
-		  ,ISNULL(od.[Label],0) AS Label
-		  ,ISNULL(ods.[Name], 'New') AS 'Status'
-		  ,ISNULL(od.[Status],0) AS StatusID 
-		  ,ISNULL((SELECT SUM(odq.[Qty]) FROM [dbo].[OrderDetailQty] odq WHERE odq.[OrderDetail] = od.[ID]), 0) AS Quantity
-		  ,ISNULL(od.[EditedPrice], 0.00) AS DistributorEditedPrice		 
-		  ,od.Surcharge 
-		  ,ISNULL(od.DistributorSurcharge,0) AS DistributorSurcharge
-		  ,od.VisualLayoutNotes
-		  ,od.EditedPriceRemarks
-		  ,od.FactoryInstructions
-		  ,vl.IsEmbroidery
-		  ,od.IsRepeat
-		  ,rf.Name AS ResolutionProfile
-		  ,pt.Name AS PocketType
-	FROM [dbo].[Order] o
-		LEFT OUTER JOIN [dbo].[OrderDetail] od
-		ON o.[ID] = od.[Order]
-		LEFT OUTER JOIN [dbo].[OrderType] ot 
-			ON od.[OrderType] = ot.[ID]	
-		LEFT OUTER JOIN [dbo].[OrderDetailStatus] ods
-			ON  od.[Status] = ods.[ID]		
-		LEFT OUTER JOIN [dbo].[VisualLayout] vl 
-			ON od.[VisualLayout] = vl.[ID]	
-		LEFT OUTER JOIN [dbo].[Pattern] p
-			ON vl.[Pattern] = p.[ID]
-		LEFT OUTER JOIN [dbo].[FabricCode] fc
-			ON vl.[FabricCode] = fc.[ID]
-		LEFT OUTER JOIN [dbo].[ResolutionProfile] rf
-			ON vl.[ResolutionProfile] = rf.[ID]	
-		LEFT OUTER JOIN [dbo].[PocketType] pt
-			ON vl.[PocketType] = pt.[ID]
-	WHERE od.[Order] = @P_Order	
-END 
+	IF OBJECT_ID('tempdb..#data') IS NOT NULL 
+		DROP TABLE #data
+
+	SELECT	vl.NamePrefix AS VLName, 
+			i.Name AS SubItemName, 
+			(ISNULL(odq.Qty, 0)) AS Quantity, 
+			((1+ (ISNULL(od.[DistributorSurcharge], 0)/100)) * ISNULL(od.EditedPrice, 0)) AS Price, 
+			(odq.Qty * (1+ (ISNULl(od.[DistributorSurcharge], 0)/100)) * ISNULL(od.EditedPrice, 0)) AS Value, 
+			(odq.Qty * (ISNULL(cs.QuotedCIF, 0) + ISNULL(od.Surcharge,0) )) AS PurchasePrice,
+			(odq.Qty * (1+ (ISNULL(od.[DistributorSurcharge], 0)/100)) * ISNULL(od.EditedPrice, 0)) - (odq.Qty * ISNULL(cs.QuotedCIF, 0)) AS GrossProfit INTO #data
+	FROM [dbo].[Company] c
+		JOIN [dbo].[Order] o
+			ON c.ID = o.Distributor
+		JOIN [dbo].[OrderDetail] od
+			ON o.ID = od.[Order]
+		JOIN [dbo].[VisualLayout] vl
+			ON od.VisualLayout = vl.ID
+		JOIN [dbo].[Pattern] p
+			ON vl.Pattern = p.ID
+		LEFT OUTER JOIN [dbo].[Item] i
+			ON p.SubItem = i.ID
+		JOIN [dbo].[JobName] j
+			ON vl.Client = j.ID
+		JOIN [dbo].[OrderDetailQty] odq
+			ON od.ID = odq.OrderDetail
+		--JOIN [dbo].[Client] cl
+		--	ON j.Client = cl.ID
+		JOIN [dbo].[CostSheet] cs
+			ON cs.Pattern = vl.Pattern
+				AND cs.Fabric = vl.FabricCode 
+	WHERE IsDistributor = 1 AND o.[Date] >= @P_StartDate AND o.[Date] <= @P_EndDate AND c.ID = @P_Distributor AND j.Client = @P_Client AND odq.Qty != 0  AND o.[Status] != 28
+
+	SELECT	d.VLName, 
+			d.SubItemName, 
+			SUM(Quantity) AS Quantity, 
+			Price, 
+			SUM(Value) AS Value, 
+			SUM(PurchasePrice) AS PurchasePrice,
+			SUM(Value - PurchasePrice) AS GrossProfit,
+			(CASE WHEN SUM(Value) = 0 THEN 0
+			ELSE 
+				SUM(Value - PurchasePrice)/SUM(Value)
+			END) AS GrossMargin
+	FROM #data d
+	GROUP BY d.VLName, d.SubItemName, Price
+	ORDER BY Value DESC
+END
 
 GO
+
 
 --**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--
---
+++ DatabaseScripts/Changes228.sql
@@ -0,0 +1,70 @@
+USE Indico
+GO
+
+--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--
+
+/****** Object:  StoredProcedure [dbo].[SPC_GetOrderDetailIndicoPrice]    Script Date: 1/26/2017 1:31:47 PM ******/
+DROP PROCEDURE [dbo].[SPC_GetOrderDetailIndicoPrice]
+GO
+
+/****** Object:  StoredProcedure [dbo].[SPC_GetOrderDetailIndicoPrice]    Script Date: 1/26/2017 1:31:47 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[SPC_GetOrderDetailIndicoPrice]
+(
+	@P_Order AS int
+)
+AS 
+
+BEGIN	
+	SELECT 	
+		  od.[ID] AS OrderDetail
+		  ,ot.[Name] AS OrderType
+		  ,ISNULL(vl.[NamePrefix] + ''+ ISNULL(CAST(vl.[NameSuffix] AS NVARCHAR(64)), ''),'') AS VisualLayout
+		  ,ISNULL(od.[VisualLayout],0) AS VisualLayoutID		  
+		  ,ISNULL(p.Number, (SELECT Number FROM Pattern WHERE ID = od.[Pattern])) AS Pattern 
+		  ,ISNULL(p.ID, od.[Pattern]) AS PatternID
+		  ,ISNULL(fc.ID, od.FabricCode) AS FabricID
+		  ,ISNULL(fc.[Name], (SELECT Name FROM FabricCode WHERE ID = od.FabricCode)) AS Fabric 		  
+		  ,od.[Order] AS 'Order'
+		  ,ISNULL(od.[Label],0) AS Label
+		  ,ISNULL(ods.[Name], 'New') AS 'Status'
+		  ,ISNULL(od.[Status],0) AS StatusID 
+		  ,ISNULL((SELECT SUM(odq.[Qty]) FROM [dbo].[OrderDetailQty] odq WHERE odq.[OrderDetail] = od.[ID]), 0) AS Quantity
+		  ,ISNULL(od.[EditedPrice], 0.00) AS DistributorEditedPrice		 
+		  ,od.Surcharge 
+		  ,ISNULL(od.DistributorSurcharge,0) AS DistributorSurcharge
+		  ,od.VisualLayoutNotes
+		  ,od.EditedPriceRemarks
+		  ,od.FactoryInstructions
+		  ,vl.IsEmbroidery
+		  ,od.IsRepeat
+		  ,rf.Name AS ResolutionProfile
+		  ,pt.Name AS PocketType
+	FROM [dbo].[Order] o
+		LEFT OUTER JOIN [dbo].[OrderDetail] od
+		ON o.[ID] = od.[Order]
+		LEFT OUTER JOIN [dbo].[OrderType] ot 
+			ON od.[OrderType] = ot.[ID]	
+		LEFT OUTER JOIN [dbo].[OrderDetailStatus] ods
+			ON  od.[Status] = ods.[ID]		
+		LEFT OUTER JOIN [dbo].[VisualLayout] vl 
+			ON od.[VisualLayout] = vl.[ID]	
+		LEFT OUTER JOIN [dbo].[Pattern] p
+			ON vl.[Pattern] = p.[ID]
+		LEFT OUTER JOIN [dbo].[FabricCode] fc
+			ON vl.[FabricCode] = fc.[ID]
+		LEFT OUTER JOIN [dbo].[ResolutionProfile] rf
+			ON vl.[ResolutionProfile] = rf.[ID]	
+		LEFT OUTER JOIN [dbo].[PocketType] pt
+			ON vl.[PocketType] = pt.[ID]
+	WHERE od.[Order] = @P_Order	
+END 
+
+GO
+
+--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--
